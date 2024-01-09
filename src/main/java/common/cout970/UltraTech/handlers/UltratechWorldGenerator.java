290ee9d73765d2657804b30d33baa8f654d35eca
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-09 21:13:46 +0100

    fix generateOil cascading worldgen

--- src/main/java/common/cout970/UltraTech/handlers/UltratechWorldGenerator.java
+++ src/main/java/common/cout970/UltraTech/handlers/UltratechWorldGenerator.java
@@ -1,10 +1,14 @@
 package common.cout970.UltraTech.handlers;
 
-import java.util.Random;
+import java.util.*;
 
+import common.cout970.UltraTech.util.BlockPos;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -19,7 +23,6 @@ public class UltratechWorldGenerator implements IWorldGenerator{
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-
 		switch(world.provider.dimensionId){
 		case -1:
 		    generateNether(world, random, chunkX * 16, chunkZ * 16);
@@ -65,18 +68,18 @@ public class UltratechWorldGenerator implements IWorldGenerator{
 
 	private void generateSurface(World world, Random random, int i, int j) {
 		Block id = BlockManager.Ores;
-		
+
 		if(OreGeneration.Aluminum)
 		genOre(world, random, i, j, OreGeneration.unitsAluminum, id, 1, OreGeneration.amountAluminum, OreGeneration.heightAluminum);
 		if(OreGeneration.Copper)
 		genOre(world, random, i, j, OreGeneration.unitsCopper, id, 2, OreGeneration.amountCopper, OreGeneration.heightCopper);
 		if(OreGeneration.Tin)
 		genOre(world, random, i, j, OreGeneration.unitsTin, id, 3, OreGeneration.amountTin, OreGeneration.heightTin);
-		
+
 		boolean lead = false,silver = false;
 		if(OreGeneration.Lead)lead = true;
 		if(OreGeneration.Silver)silver = true;
-		
+
 		for(int k = 0; k < OreGeneration.unitsLead; k++) {
 			int firstBlockXCoord = i + random.nextInt(16);
 			int firstBlockYCoord = random.nextInt(OreGeneration.heightLead);
@@ -87,9 +90,9 @@ public class UltratechWorldGenerator implements IWorldGenerator{
 			(new WorldGenMinable(id,5,OreGeneration.amountLead,Blocks.stone)).generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
 		}
 	}
-	
+
 	public void genOre(World world, Random random, int i, int j, int units, Block id, int meta, int amount, int maxheight){
-		
+
 		for(int k = 0; k < units; k++) {
 			int firstBlockXCoord = i + random.nextInt(16);
 			int firstBlockYCoord = random.nextInt(maxheight);
@@ -97,7 +100,7 @@ public class UltratechWorldGenerator implements IWorldGenerator{
 			(new WorldGenMinable(id,meta,amount,Blocks.stone)).generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
 		}
 	}
-	
+
 	public void generateShip(Random r,int x, int y,World w){
 		System.out.println("gen");
 		int j = 150;//+ r.nextInt(20);
@@ -111,38 +114,57 @@ public class UltratechWorldGenerator implements IWorldGenerator{
 			}
 		}
 	}
-	
 
-	private void generateOil(Random r, int x, int y, World w) {
-		
-		int j = 10+ r.nextInt(20);
-		boolean gen = false;
-		for(int h = j-5;h<j+5;h++){
-			Block b = w.getBlock(x, h, y);
-			if(b == Blocks.stone){
-				gen = true;
-				break;
-			}
-		}
-		if(!gen)return;
-		int ran = r.nextInt(100);
-		int t = 7;
-		if(ran%2 == 0)t = 10;
-		if(ran%7 == 0)t = 18;
-		for(int l =-t/4-4;l<t/4+4;l++){
-			for(int s=-t;s<t+1;s++)
-				for(int d=-t*4;d<t*4+1;d++){
-					double b = Math.sqrt(s*s+d*d*0.25+l*l*4);
-					b = Math.abs(b);
-					if(b<t)w.setBlock(x+s, j+l, y+d,FluidRegistry.getFluid("oil").getBlock(), 0, 6);
-						else if(b < t+5){
-						w.setBlock(x+s, j+l, y+d,Blocks.stone, 0, 6);
-					}
-				}
-		}
-		int max = w.getHeightValue(x, y)+6;
-		for(int h=j;h<max;h++){
-			w.setBlock(x, h, y,FluidRegistry.getFluid("oil").getBlock(), 0, 6);
-		}
-	}
+
+    private void generateOil(Random r, int x, int y, World w) {
+        int chunkX = x >> 4;
+        int chunkZ = y >> 4;
+        Chunk chunk = w.getChunkFromChunkCoords(chunkX, chunkZ);
+
+        if (!chunk.isChunkLoaded) {
+            return;
+        }
+        System.out.println("Generating oil at coordinates X: " + x + ", Y: " + y);
+        int j = 10 + r.nextInt(20);
+        boolean gen = false;
+        Set<ChunkCoordinates> oilBlocks = new HashSet<>();
+
+        for (int h = j - 5; h < j + 5; h++) {
+            Block b = chunk.getBlock(x & 15, h, y & 15);
+            if (b == Blocks.stone) {
+                gen = true;
+                break;
+            }
+        }
+
+        if (!gen) {
+            return;
+        }
+
+        int ran = r.nextInt(100);
+        int t = (ran % 7 == 0) ? 18 : ((ran % 2 == 0) ? 10 : 7);
+
+        for (int l = -t / 4 - 4; l < t / 4 + 4; l++) {
+            for (int s = -t; s < t + 1; s++) {
+                for (int d = -t * 4; d < t * 4 + 1; d++) {
+                    double distance = Math.sqrt(s * s + d * d * 0.25 + l * l * 4);
+                    distance = Math.abs(distance);
+                    if (distance < t && (chunk.getBlock(x + s & 15, j + l, y + d & 15) == Blocks.stone)) {
+                        oilBlocks.add(new ChunkCoordinates(x + s, j + l, y + d));
+                    }
+                }
+            }
+        }
+
+        int max = w.getHeightValue(x, y) + 6;
+        for (int h = j; h < max; h++) {
+            if (chunk.getBlock(x & 15, h, y & 15) == Blocks.stone) {
+                oilBlocks.add(new ChunkCoordinates(x, h, y));
+            }
+        }
+
+        for (ChunkCoordinates pos : oilBlocks) {
+            chunk.func_150807_a(pos.posX & 15, pos.posY, pos.posZ & 15, FluidRegistry.getFluid("oil").getBlock(), 0);
+        }
+    }
 }
